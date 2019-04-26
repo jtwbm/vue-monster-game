a832652cc6e50da0ba4bc2976ce4a017346e94e8
jtwbm <jukaenerdjaizer@gmail.com>
2019-04-26 07:23:13 +0300

    init players

--- src/components/Default.vue
+++ src/components/Default.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    {{ msg }}
+	<div>
+  		<div class="players">
+		    <div class="player">
+		    	<div class="player__name">You</div>
+		    	<div class="player__xp"></div>
+		    </div>
+		    <div class="player">
+		    	<div class="player__name">Monster</div>
+		    	<div class="player__xp"></div>
+		    </div>
+		</div>
+		<div class="actions">
+			<button class="button">Обычный удар</button>
+			<button class="button">Супер удар</button>
+			<button class="button">Подлечиться</button>
+		</div>
   </div>
 </template>
 
@@ -15,5 +29,27 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss">
+.players {
+	text-align: center;
+}
+.player {
+	display: inline-block;
+	vertical-align: top;
+	width: 40%;
+	margin: 0 2rem;
+
+	&__name {
+		text-transform: uppercase;
+		font-size: 4rem;
+		font-weight: 700;
+		margin-bottom: 2rem;
+		line-height: 1;
+	}
+	&__xp {
+		display: block;
+		height: 1rem;
+		background: green;
+	}
+}
 </style>
