067ad90de8c14ecc842dc2b3a25fa9fd5a8ec82d
jtwbm <jukaenerdjaizer@gmail.com>
2019-04-27 07:00:20 +0300

    add some styles

--- src/components/Default.vue
+++ src/components/Default.vue
@@ -1,20 +1,30 @@
 <template>
 	<div>
+		<div class="gameActions">
+			<p class="gameActions__player">Игрок атаковал монстра обычным ударом: -3HP</p>
+			<p class="gameActions__monster">Монстр применил комбо: -12HP</p>
+		</div>
   		<div class="players">
 		    <div class="player">
 		    	<div class="player__name">You</div>
-		    	<div class="player__xp"></div>
+		    	<div class="player__line"><div class="player__hp"></div><span>Здоровье</span></div>
+		    	<div class="player__line"><div class="player__mp"></div><span>Мана</span></div>
+
+		    	<div class="actions">
+		    		<div class="actions__title">Атаки</div>
+					<button class="button default-color">Обычный удар</button>
+					<button class="button combo-color">Комбо</button>
+					<button class="button magic-color">Заклинание</button>
+					<button class="button heal-color">+20HP</button>
+				</div>
 		    </div>
 		    <div class="player">
 		    	<div class="player__name">Monster</div>
-		    	<div class="player__xp"></div>
+		    	<div class="player__line"><div class="player__hp"></div><span>Здоровье</span></div>
+		    	<div class="player__line"><div class="player__mp"></div><span>Мана</span></div>
 		    </div>
 		</div>
-		<div class="actions">
-			<button class="button">Обычный удар</button>
-			<button class="button">Супер удар</button>
-			<button class="button">Подлечиться</button>
-		</div>
+		
   </div>
 </template>
 
@@ -46,10 +56,113 @@ export default {
 		margin-bottom: 2rem;
 		line-height: 1;
 	}
-	&__xp {
-		display: block;
-		height: 1rem;
-		background: green;
+	&__line {
+		height: 3rem;
+		position: relative;
+		background-color: #ccc;
+		margin-bottom: .5rem;
+
+		span {
+			display: block;
+			color: #fff;
+			font-size: 1.2rem;
+			line-height: 3rem;
+			height: 100%;
+			text-align: left;
+			z-index: 10;
+			position: relative;
+			padding: 0 2rem;
+		}
+	}
+	&__hp,
+	&__mp {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+	}
+	&__hp {
+		background-color: green;
+	}
+	&__mp {
+		background-color: blue;
+	}
+}
+.actions {
+	display: flex;
+	justify-content: space-between;
+	align-items: stretch;
+	margin-top: 2rem;
+	flex-wrap: wrap;
+
+	.button {
+		flex: 1 1 20%;
+	}
+	&__title {
+		text-transform: uppercase;
+		color: #333;
+		margin-bottom: 1rem;
+		flex-basis: 100%;
+	}
+}
+.button {
+	cursor: pointer;
+	color: #fff;
+	border: none;
+	font-size: 1.4rem;
+	transition: all .3s;
+	padding: 1rem;
+
+	&:focus {
+		outline: none;
+		box-shadow: inset 0 0 .5rem 0 rgba(0,0,0,.3);
+	}
+	&.default-color {
+		background-color: #ffaa0a;
+
+		&:hover {
+			background-color: #ffaa0a + #111;
+		}
+	}
+	&.combo-color {
+		background-color: #ff0a0a;
+
+		&:hover {
+			background-color: #ff0a0a + #111;
+		}
+	}
+	&.magic-color {
+		background-color: #c029e4;
+
+		&:hover {
+			background-color: #c029e4 + #111;
+		}
+	}
+	&.heal-color {
+		background-color: #1bd488;
+
+		&:hover {
+			background-color: #1bd488 + #111;
+		}
+	}
+}
+.gameActions {
+	text-align: center;
+	margin: 3rem auto;
+
+	p {
+		margin: 0 0 1rem;
+
+		&:empty {
+			display: none;
+		}
+	}
+	&__player {
+		color: green;
+	}
+	&__monster {
+		color: red;
 	}
 }
 </style>
